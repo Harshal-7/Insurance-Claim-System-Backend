7afc61dc52d31716c6a9e0e3991ee3498f12d00e
Harshal Shinde <harshal@vexio.in>
2024-12-20 11:21:49 +0530

    env variables added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { PoliciesModule } from './policies/policies.module';
 import { ClaimsModule } from './claims/claims.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: '',
+      password: process.env.MY_SQL_PASSWORD,
       database: 'insurance_system',
       autoLoadEntities: true,
       synchronize: true,
